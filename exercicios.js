bb172cdda7ef93ea3dd5a4a8a61b1acf89d0354a
Vinícius Raniery <124690508+V1niciusR@users.noreply.github.com>
2023-03-06 16:24:06 -0300

    Update exercicios.js

--- exercicios.js
+++ exercicios.js
@@ -1,17 +1,25 @@
-/*Leia várias ideias e calcule a média entre as idades*/
+/* Leia várias ideias e calcule a média entre as idades */
 
-/*Ler as idades*/
+/* Ler as idades */
 const prompt = require("prompt-sync")();
 
-/*Prompt retorna uma string, logo precisamos converter para número*/
+/* Prompt retorna uma string, logo precisamos converter para número */
 const numeroIdades=prompt("Quantas idades precisamos ler?");
 
 const listaIdades= new Array(Number(numeroIdades));
 
 let i = 0;
-let tamnhoLista = listaIdades.length;
-while(i < tamnhoLista){
+let tL = listaIdades.length;
+while(i < tL){
     let idade = prompt(`Qual a idade núemero ${i}`);
     listaIdades[i]=Number(idade);
     i++;
+}
+
+/* Cálculo média aritmética */
+/* Já tenho um índice, vamos reutilizar a variável e zerar ela */
+i = 0;
+while(i < tL){
+    let somaItemLista = 0;
+    somaItemLista = somaItemLista + listaIdades[indice]; 
 }
