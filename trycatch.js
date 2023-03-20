296857944522011c358445bbfe2f5064159997fc
Vinícius Raniery <124690508+V1niciusR@users.noreply.github.com>
2023-03-20 16:07:26 -0300

    Update trycatch.js

--- trycatch.js
+++ trycatch.js
@@ -1,16 +1,16 @@
-//Tratamento de erros e exceções try catch e finally
+/* Tratamento de erros e exceções try catch e finally */
 
 try {
-    //código que pode lançar um erro 
+    /* código que pode lançar um erro */
     let x = y + 1; //y não está definido
 } 
 
 catch (err) {
-    //código que executo se um erro for lançado
+    /* código que executo se um erro for lançado */
     console.error(err); //imprime o objeto de erro no console
 } 
 
 finally {
-    //código que executa depois do try ou catch 
+    /* código que executa depois do try ou catch */
     console.log('Fim do exemplo'); //imprime uma mensagem 
 }
