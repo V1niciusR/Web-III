b11c62d596dfcb2893461739c656ef9460df95c4
Vinícius Raniery <124690508+V1niciusR@users.noreply.github.com>
2023-03-20 16:06:05 -0300

    Update trycatch.js

--- trycatch.js
+++ trycatch.js
@@ -3,10 +3,14 @@
 try {
     //código que pode lançar um erro 
     let x = y + 1; //y não está definido
-} catch (err) {
+} 
+
+catch (err) {
     //código que executo se um erro for lançado
     console.error(err); //imprime o objeto de erro no console
-} finally {
+} 
+
+finally {
     //código que executa depois do try ou catch 
     console.log('Fim do exemplo'); //imprime uma mensagem 
 }
