27544cc84f9d97ba05b8e056b30db9c7ce6c351d
Vinícius Raniery <124690508+V1niciusR@users.noreply.github.com>
2023-05-17 00:28:30 -0300

    Nova pasta

---
+++ Trimestre_2/index.js
@@ -0,0 +1,15 @@
+const mysql = require('mysql2/promise')
+
+// Crie uma conexão com o banco de dados MySQL
+// create the connection to database
+try {
+  const connection =  mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'vinicius'
+  });
+  console.log('Conexão com o banco de dados estabelecida!')
+} catch (error) {
+  console.log(`Não foi possível estabelecer conexão com o banco de dados. Erro: ${error}`)
+}
+
