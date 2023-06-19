cc285bd8abb3d7e6715c30492d636a950a3bdbd6
Vinícius Raniery <124690508+V1niciusR@users.noreply.github.com>
2023-06-19 14:10:56 -0300

    Teste do código

--- Trimestre_2/index.js
+++ Trimestre_2/index.js
@@ -22,11 +22,11 @@ try {
 
 /* INSERIR E APAGAR DADOS */
 
-Apagar(5);
-Apagar(6);
-Apagar(7);
-Apagar(8);
-Apagar(9);
+Inserir("João", 20);
+Inserir("Maria", 18);
+Inserir("José", 19);
+Inserir("Ana", 21);
+
 
 /* /INSERIR E APAGAR DADOS */
 
