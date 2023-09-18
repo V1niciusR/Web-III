52a1348791c71f6673c59ae6b97b19d76f35d960
Vinícius Raniery <124690508+V1niciusR@users.noreply.github.com>
2023-09-18 14:43:29 -0300

    ...

---
+++ Trimestre_3/Cadastro_CPF.js
@@ -0,0 +1,181 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const professor = require('./DAO/professor');
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send(`
+    <h1>Página principal</h1>
+    <h3>Cadastro Professor</h3>
+    <ul>
+      <li><a href="/inserir">Inserir</a></li>
+      <li><a href="/apagar">Apagar</a></li>
+      <li><a href="/listar">Listar</a></li>
+      <li><a href="/atualizar">Atualizar</a></li>
+    </ul>
+  `);
+});
+
+app.get('/sucesso', (req, res) => {
+    res.send(`
+    <h1 style="color:green;">Operação executada com sucesso ;)</h1>
+    <a href="/">Voltar</a>
+  `);
+});
+
+app.get('/erro', (req, res) => {
+    res.send(`
+    <h1 style="color:red;">Ops...aconteceu algum problema</h1>
+    <a href="/">Voltar</a>
+  `);
+});
+
+app.get('/inserir', (req, res) => {
+    res.send(`
+    <ul>
+      <li><a href="/">Principal</a></li>
+      <li><a href="/apagar">Apagar</a></li>
+      <li><a href="/atualizar">Atualizar</a></li>
+      <li><a href="/listar">Listar</a></li>
+    </ul>
+    <h1>Inserir Professor</h1>
+    <form action="/inserirprofessor" method="post">
+      <label>CPF:</label>
+      <input type="text" name="cpf" required><br><br>
+      <label>Nome:</label>
+      <input type="text" name="nome" required><br><br>
+      <label>Titulação:</label>
+      <input type="text" name="titulacao" required><br><br>
+      <input type="submit" value="Inserir">
+    </form>
+  `);
+});
+
+app.post('/inserirprofessor', (req, res) => {
+    const cpf = req.body.cpf;
+    const nome = req.body.nome;
+    const titulacao = req.body.titulacao;
+
+    professor.Inserir(cpf, nome, titulacao, (err, rows) => {
+        if (err) {
+            console.error(err);
+            res.redirect('/erro');
+        } else {
+            if (rows > 0) {
+                res.redirect('/sucesso');
+            } else {
+                res.redirect('/erro');
+            }
+        }
+    });
+});
+
+app.get('/listar', (req, res) => {
+    professor.ListarTodosProfessores((err, results) => {
+        if (err) {
+            console.error(err);
+            res.redirect('/erro');
+        } else {
+            res.send(`
+        <h1>Lista de Professores</h1>
+        <ul>
+          <li><a href="/inserir">Inserir Professor</a></li>
+          <li><a href="/apagar">Apagar Professor</a></li>
+          <li><a href="/atualizar">Atualizar Professor</a></li>
+        </ul>
+        <br>
+        <table>
+          <tr>
+            <th>ID</th>
+            <th>CPF</th>
+            <th>Nome</th>
+            <th>Titulação</th>
+          </tr>
+          ${results
+                    .map(
+                        (prof) =>
+                            `<tr><td>${prof.id}</td><td>${prof.cpf}</td><td>${prof.nome}</td><td>${prof.titulacao}</td></tr>`
+                    )
+                    .join('')}
+        </table>
+      `);
+        }
+    });
+});
+
+app.get('/apagar', (req, res) => {
+    res.send(`
+    <ul>
+      <li><a href="/">Principal</a></li>
+      <li><a href="/inserir">Inserir</a></li>
+      <li><a href="/listar">Listar</a></li>
+      <li><a href="/atualizar">Atualizar</a></li>
+    </ul>
+    <h1>Apagar Professor</h1>
+    <form action="/apagarprofessor" method="post">
+      <label>ID do Professor:</label>
+      <input type="number" name="id" required><br><br>
+      <input type="submit" value="Apagar">
+    </form>
+  `);
+});
+
+app.post('/apagarprofessor', (req, res) => {
+    const idProfessor = req.body.id;
+
+    professor.Apagar(idProfessor, (err, result) => {
+        if (err) {
+            console.error(err);
+            res.redirect('/erro');
+        } else {
+            if (result.affectedRows > 0) {
+                res.redirect('/sucesso');
+            } else {
+                res.redirect('/erro');
+            }
+        }
+    });
+});
+
+app.get('/atualizar', (req, res) => {
+    res.send(`
+    <ul>
+      <li><a href="/">Principal</a></li>
+      <li><a href="/inserir">Inserir</a></li>
+      <li><a href="/listar">Listar</a></li>
+    </ul>
+    <h1>Atualizar Professor</h1>
+    <form action="/atualizarprofessor" method="post">
+      <label>ID do Professor:</label>
+      <input type="number" name="id" required><br><br>
+      <label>Novo Nome:</label>
+      <input type="text" name="novoNome" required><br><br>
+      <input type="submit" value="Atualizar">
+    </form>
+  `);
+});
+
+app.post('/atualizarprofessor', (req, res) => {
+    const idProfessor = req.body.id;
+    const novoNome = req.body.novoNome;
+
+    professor.Atualizar(idProfessor, novoNome, (err, result) => {
+        if (err) {
+            console.error(err);
+            res.redirect('/erro');
+        } else {
+            if (result.affectedRows > 0) {
+                res.redirect('/sucesso');
+            } else {
+                res.redirect('/erro');
+            }
+        }
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
---
+++ Trimestre_3/DAO/professor.js
@@ -0,0 +1,110 @@
+const mysql = require('mysql2');
+
+let connection;
+
+try {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        database: 'professor01'
+    });
+} catch (error) {
+    error.stack = '';
+    console.error(error.message);
+}
+
+function validarCPF(cpf) {
+    cpf = cpf.replace(/[^\d]+/g, ''); // Remove todos os caracteres não numéricos
+    if (cpf.length !== 11 || /^(\d)\1+$/.test(cpf)) {
+        throw new Error('CPF inválido: O CPF deve conter 11 dígitos distintos.');
+    }
+    let soma = 0;
+    for (let i = 0; i < 9; i++) {
+        soma += parseInt(cpf.charAt(i)) * (10 - i);
+    }
+    const resto = soma % 11;
+    let digitoVerificador1 = 11 - resto;
+    if (digitoVerificador1 > 9) {
+        digitoVerificador1 = 0;
+    }
+    if (parseInt(cpf.charAt(9)) !== digitoVerificador1) {
+        throw new Error('CPF inválido: Dígito verificador 1 não corresponde.');
+    }
+    soma = 0;
+    for (let i = 0; i < 10; i++) {
+        soma += parseInt(cpf.charAt(i)) * (11 - i);
+    }
+    const resto2 = soma % 11;
+    let digitoVerificador2 = 11 - resto2;
+    if (digitoVerificador2 > 9) {
+        digitoVerificador2 = 0;
+    }
+    if (parseInt(cpf.charAt(10)) !== digitoVerificador2) {
+        throw new Error('CPF inválido: Dígito verificador 2 não corresponde.');
+    }
+}
+
+
+function Inserir(cpf, nome, titulacao, callback) {
+    console.log('CPF recebido:', cpf); // Adicione esta linha para depuração
+    try {
+        validarCPF(cpf);
+        connection.query(
+            'INSERT INTO professor (cpf, nome, titulacao) VALUES (?, ?, ?)',
+            [cpf, nome, titulacao],
+            function (err, results) {
+                if (err) {
+                    callback(err, null);
+                } else {
+                    callback(null, results.affectedRows);
+                }
+            }
+        );
+    } catch (error) {
+        callback(error, null);
+    }
+}
+
+
+function ListarTodosProfessores(callback) {
+    connection.query(
+        'SELECT * FROM professor',
+        function (err, results) {
+            if (err) {
+                callback(err, null);
+            } else {
+                callback(null, results);
+            }
+        }
+    );
+}
+
+function Apagar(idProfessor, callback) {
+    connection.query(
+        'DELETE FROM professor WHERE id = ?',
+        [idProfessor],
+        function (err, result) {
+            if (err) {
+                callback(err, null);
+            } else {
+                callback(null, result);
+            }
+        }
+    );
+}
+
+function Atualizar(idProfessor, novoNome, callback) {
+    connection.query(
+        'UPDATE professor SET nome = ? WHERE id = ?',
+        [novoNome, idProfessor],
+        function (err, result) {
+            if (err) {
+                callback(err, null);
+            } else {
+                callback(null, result);
+            }
+        }
+    );
+}
+
+module.exports = { Inserir, ListarTodosProfessores, Apagar, Atualizar };
